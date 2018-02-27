e55906dd1844baaf11e087d263708082d11a6695
Nicolás Arias <nmarias38@misena.edu.co>
2018-02-27 16:48:47 -0500

    api rest

---
+++ api/Controllers/usersCtrl.php
@@ -0,0 +1,60 @@
+<?php
+class Usuario {
+    private $cnx;
+    public $response;
+    public function __construct($cnx, $action) {
+        header('Allow-Methods: POST');
+        $this->cnx = $cnx;
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method == 'POST') {
+            switch ($action) {
+                case 'create':
+                $this->create();
+                break;
+            case 'login':
+                $this->login();
+                break;
+            default:
+                $this->displayError("No existe la acción $action");
+                break;
+            }
+        } else $this->displayError('La creación de usuarios solo permite el método POST');
+    }
+    private function create() {
+        if (isset($_POST['name']) && isset($_POST['lastName']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['phone'])) {
+            $password = md5($_POST['password']);
+            $query = 'INSERT INTO users SET name=:nombre, last_name=:apellido, email=:email, password=:password, phone=:telefono';
+            $sentence = $this->cnx->prepare($query);
+            $sentence->bindParam(':nombre', $_POST['name']);
+            $sentence->bindParam(':apellido', $_POST['lastName']);
+            $sentence->bindParam(':email', $_POST['email']);
+            $sentence->bindParam(':password', $password);
+            $sentence->bindParam(':telefono', $_POST['phone']);
+            if($sentence->execute()) {
+                $this->response = array(
+                    'status' => 200,
+                    'message' => 'Usuario creado con éxito'
+                );
+            } else $this->displayError('Error al ingresar el usuario');
+        } else $this->displayError('Todos los campos son requeridos');
+    }
+    public function login () {
+        $email = $_POST['email'];
+        $password = md5($_POST['password']);
+        $query = 'SELECT * FROM users WHERE email=:email AND password=:pasword';
+        $this->cnx->prepare($query);
+        $this->cnx->bindParam(':email', $email);
+        $this->cnx->bindParam(':password', $password);
+        $result =$this->cnx->execute();
+        if($result) {
+            $result = $result->fetchAll(PDO::FETCH_ASSOC);
+            $this->response = $result;
+        }
+    }
+    private function displayError($message) {
+        $this->response = array(
+            'status' => 400,
+            'message' => $message
+        );
+    }
+}
---
+++ api/index.php
@@ -0,0 +1,25 @@
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include_once 'Conexion.php';
+$route = $_GET['PATH_INFO'];
+$route = explode('/', $route);
+$resource = $route[0];
+$action = $route[1];
+$response = null;
+switch ($resource) {
+    case 'usuarios':
+        $db = new Database();
+        $cnx = $db->cnx;
+        include_once 'Controllers/usersCtrl.php';
+        $instance = new Usuario($cnx, $action);
+    break;
+    default: 
+     $response = array(
+         'status' => 400,
+         'message' => "No existe el recurso $resource"
+     );
+}
+if($instance) $response = $instance->response;
+echo json_encode($response);
