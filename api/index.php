01f439d3763a25bab271cfa445db9a5522e2521d
Nicolás Arias <nmarias38@misena.edu.co>
2018-03-01 14:38:08 -0500

    cambios

--- api/index.php
+++ api/index.php
@@ -1,7 +1,8 @@
 <?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+header('Access-Control-Allow-Origin: *');
 include_once 'Conexion.php';
 $route = $_GET['PATH_INFO'];
 $route = explode('/', $route);
