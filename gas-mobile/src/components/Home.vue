379fc71f212ea51c2599a3f9f434869def8cda88
Nicolás Arias <nmarias38@misena.edu.co>
2018-03-10 08:50:42 -0500

    cambios

--- gas-mobile/src/components/Home.vue
+++ gas-mobile/src/components/Home.vue
@@ -1,9 +1,9 @@
 <template>
     <Page class="first-page" actionBarHidden="true" backgroundSpanUnderStatusBar="true">
-         <FlexboxLayout flexDirection="column" justifyContent="space-between">
-            <StackLayout class="logo">
+         <FlexboxLayout flexDirection="column" justifyContent="center">
+            <!-- <StackLayout class="logo">
             <Image src="~/images/logo.jpg"/>
-            </StackLayout>
+            </StackLayout> -->
             <StackLayout class="buttons">
                 <button @tap="$router.push('/panel')" class="btn-entrar" >Entrar</button>
                 <button @tap="$router.push('/login')" class=" btn white">Iniciar Sesion</button>
@@ -28,6 +28,7 @@ Page {
     padding: 0;
 }
 .logo {
+    margin-top: 70;
     padding: 40;
     background: #fff;
 }
@@ -35,14 +36,14 @@ Page {
     padding: 15;
 }
 .btn-entrar{
-    background-color: #40739e;
+    background-color: #26970f;
 }
 .btn-entrar:pressed{
     background-color: #175e99;
 }
 .white {
-    background-color: #fff;
-    color: #000;
+    background-color: #40739e;
+    color: #fff;
 }
 .white:pressed{
     background-color: rgb(165, 164, 164);
--- gas-mobile/src/components/Panel.vue
+++ gas-mobile/src/components/Panel.vue
@@ -3,11 +3,11 @@
     <ActionBar title="Publiservicios" flat="true"/>      
     <StackLayout orientation="vertical">
       <Image src="~/images/stove.jpg"/>
-       <ListView for="item in items" @itemTap="onItemTap" >
+       <ListView for="item in items" @itemTap="onItemTap"  >
         <v-template>
-          <StackLayout class="list-item" orientation="horizontal">
-            <Label :text="item.icon" class="icon"/>
-            <Label :text="item.text"/>
+          <StackLayout class="list-item" orientation="horizontal" >
+            <Label :text="item.icon" class="icon" />
+            <Label :text="item.text" />
           </StackLayout>
         </v-template>
       </ListView> 
@@ -16,16 +16,18 @@
   </Page>
 </template>
 <script>
+  import {mapState} from 'vuex'
   export default {
     created () {
-      console.log(this.$store)
+      console.log(this.userData)
     },
+    computed: mapState(['userData']),
     data(){
       return{
         items :[
           {icon: String.fromCharCode('0xe903'), text: 'Beneficios del gas natural', route: '/beneficios'},
-          {icon: 'e991', text: 'Procesos y costos de instalación', route: '/instalacion'},
-          {icon: 'ea08', text: 'Reportar un problema con el gas natural', route: '/home'}
+          {icon: String.fromCharCode('0xe991'), text: 'Procesos y costos de instalación', route: '/instalacion'},
+          {icon: String.fromCharCode('0xea08'), text: 'Reportar un problema con el gas natural', route: '/home'}
         ]
       }
     },
@@ -42,9 +44,10 @@
     color:#fff;
   }
   .list-item {
-    padding: 10;
+    padding: 14;
+    font-size: 18; 
+    color: #000;
   }
-  .list-item Label {
-    font-size: 18;
-  }
+
+
 </style>
